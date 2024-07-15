dd56d2ad28572bd436583d2147e32b45cd0f5e6f
Moonie <doragondoshiro@gmail.com>
2024-07-15 17:04:42 -0400

    docs: name changes

--- obj/Debug/net8.0/gente.AssemblyInfo.cs
+++ obj/Debug/net8.0/gente.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gente")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82dae8aafd7296135733a1b7e960c4b74f9e403")]
 [assembly: System.Reflection.AssemblyProductAttribute("gente")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gente")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
